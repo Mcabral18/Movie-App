e0e758cc2527f9c01d86085704765f2b21faa7b3
Mcabral18 <cabralovil@gmail.com>
2020-11-29 23:37:34 +0000

    ola

---
+++ movie_app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
